9bb6fbf11636c3bda18dd52cb4d6e48c1b1e0d83
chio-nzgft <chio@nzgft.com>
2016-12-14 11:24:17 +0800

    Create wan1.php

---
+++ wan1.php
@@ -0,0 +1,15 @@
+<?php
+$http="http://12.12.12.12:8090/speed.php";
+$buffer = file($http);
+for($i=0;$i<sizeof($buffer);$i++)
+{
+ $n1=strpos(" ".$buffer[$i],"<title>");
+if($n1>0)
+ {
+  $n2=strrpos($buffer[$i],"</title>");
+  $title=substr($buffer[$i],$n1+7,$n2-$n1-7);
+  $title=iconv("UTF-8","big5",$title);
+  echo $title;
+ }
+}
+?>
