310284291f2d48756194463d736225139b6fca35
chio-nzgft <chio@nzgft.com>
2016-12-15 10:08:13 +0800

    Update wan1-speed-upload.pl

--- wan1-speed-upload.pl
+++ wan1-speed-upload.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 $a= `curl http://cacti-nz.rhclude.com/wan1.php`;
 $a=~ tr/\n\r\z //d;
-if ($a eq "") { $a=1 ;  print "speed:$a"}
+if ($a eq "") { $a=0 ;  print "speed:$a"}
 else {print $a; }
